2fe559bbe087a899a0a160d719e17de8afafcac2
Sulik Szabolcs <sulik.szabolcs@gmail.com>
2010-01-06 17:19:55 +0100

    must have put back into the core

--- greebo.php
+++ greebo.php
@@ -6,22 +6,19 @@ class Greebo
 {
   private $_container;
   
-  function __construct(Container $container)
-  {
+  function __construct(Container $container) {
     $this->_container = $container;
   }
   
-  static function create(Container $container)
-  {
-    return new self($container);
-  }
-  
-  function unleash()
-  {
+  function unleash() {
+    $this->_container->hooks->fire('startup' $this->_container);
+    
     $controller = $this->_container->controller;
     $controller();
     
     $this->_container->response->send();
+    
+    $this->_container->hooks->fire('shutdown' $this->_container);
   }
 }
 
@@ -32,21 +29,43 @@ class Container
 {
   private $_services = array();
   
-  function __set($id, $val)
-  {
+  function __construct() {
+    $this->init();
+  }
+  
+  function init() {
+    $this->vendor = 'Greebo';
+    $this->app = 'default';
+    $this->request = $this->shared(function($c) { return new Request($c); });
+    $this->response = $this->shared(function($c) { return new Response($c); });
+    $this->hooks = $this->shared(function($c) { return new Hooks; });
+    $this->action = $this->request->param('action', 'index');
+    $this->controller = $this->shared(function($c) {
+      $class = sprintf('%s\\Controller\\%s', $c->vendor, $c->app);
+      return new $class($c);
+    });
+    $this->template = $this->shared(function($c) {
+      $class = sprintf('%s\\Template\\%s\\%s', $c->vendor, $c->app, $c->action);
+      $class = new $class($c);
+      $class->escaper(function($val) { return htmlentities($val, ENT_QUOTES, 'utf-8'); });
+      return $class;
+    });
+  }
+  
+  function __set($id, $val) {
     $this->_services[$id] = $val;
   }
   
-  function __get($id)
-  {
+  function __get($id) {
     return is_callable($this->_services[$id]) ? $this->_services[$id]($this) : (@$this->_services[$id] ?: null);
   }
   
-  function shared($callable)
-  {
+  function shared($callable) {
     return function ($c) use ($callable) {
       static $object;
-      if (!$object) $object = $callable($c);
+      if (!$object) {
+        $object = $callable($c);
+      }
       return $object;
     };
   }
@@ -56,21 +75,20 @@ class Hooks
 {
   private $_hooks = array();
   
-  function reg($hook, $callable)
-  {
+  function reg($hook, $callable) {
     $this->_hooks[$hook][] = $callable;
   }
   
-  function fire($hook, $subject)
-  {
-    foreach ($this->_hooks[$hook] as $callable)
+  function fire($hook, $subject) {
+    foreach ($this->_hooks[$hook] as $callable) {
       $callable($subject);
+    }
   }
   
-  function filter($hook, $subject, $content)
-  {
-    foreach ($this->_hooks[$hook] as $callable)
+  function filter($hook, $subject, $content) {
+    foreach ($this->_hooks[$hook] as $callable) {
       $content = $callable($subject, $content);
+    }
     return $content;
   }
 }
@@ -79,48 +97,65 @@ class Base
 {
   private $_container;
 
-  function __construct(Container $container)
-  {
+  function __construct(Container $container) {
     $this->_container = $container;
     $this->init();
   }
   
   function init() {}
 
-  function __get($name)
-  {
+  function __get($name) {
     return $this->_container->$name;
+  }
+
+  function __set($name, $val) {
+    $this->_container->$name = $val;
   }
 }
 
 class Controller extends Base
 {
-  function __invoke()
-  {
-    $result = (method_exists($this, $method = $this->action.'Action')) ? $this->$method() : null;
-    
-    if (false !== $result && $this->template)
-      $this->response->content($this->template->fetch());
+  function __invoke() {
+    try {
+      if (!method_exists($this, $method = $this->action.'Action')) {
+        $this->forward('error404');
+      }
+       
+      if (false !== $this->$method() && $this->template) {
+        $this->response->content($this->template->fetch());
+      }
+    } catch (ForwardException $e) {
+      $this();
+    }
+  }
+  
+  function error404Action() {
+    $this->response->status(404);
+    $this->response->content('Error 404 Page');
+    return false;
+  }
+  
+  function forward($action) {
+    $this->action = 'error404';
+    throw new ForwardException;
   }
 }
 
 class Request extends Base
 {
-  function header($name, $def = null)
-  {
+  function header($name, $def = null) {
     return @$_SERVER[strtoupper($name)] ?: $def;
   }
   
-  function param($name, $def = null)
-  {
+  function param($name, $def = null) {
     $param = $_GET;
-    if ($this->header('request_method') == 'POST')
+    if ($this->header('request_method') == 'POST') {
       $param = array_merge($param, $_POST);
+    }
     return @$param[$name] ?: $def;
   }
   
-  function __call($method, $args)
-  {
+  function __call($method, $args) {
     $param = @$GLOBALS['_'.strtoupper($method)];
     return @$param[$args[0]] ?: @$args[1];
   }
@@ -134,83 +169,77 @@ class Response extends Base
     $_status = 200,
     $_content = null;
   
-  function status($status)
-  {
+  function status($status) {
     $this->_status = (int)$status;
   }
   
-  function header($name, $val)
-  {
+  function header($name, $val) {
     $this->_header[$name] = $val;
   }
   
-  function cookie($val)
-  {
+  function cookie($val) {
     $this->_cookie[] = $val;
   }
   
-  function content($content)
-  {
+  function content($content) {
     $this->_content = $content;
   }
   
-  function send()
-  {
+  function send() {
     header($this->request->header('SERVER_PROTOCOL').' '.$this->_status);
-    foreach ($this->_header as $name => $header)
+    foreach ($this->_header as $name => $header) {
       header("$name: $header");
-    foreach ($this->_cookie as $cookie)
+    }
+    foreach ($this->_cookie as $cookie) {
       call_user_func_array('setrawcookie', $cookie);
-    if (null !== $this->_content)
-      echo $this->_content;
+    }
+    if (null !== $this->_content) {
+      echo $this->hooks->filter('response.content', $this, $this->_content);
+    }
   }
 }
 
-class Template
+class Template extends Base
 {
   private
     $_slots = array(),
     $_slot,
     $_escaper;
   
-  function __get($slot)
-  {
+  function __get($slot) {
     return @$this->_slots[$slot] ?: null;
   }
   
-  function __set($slot, $val)
-  {
+  function __set($slot, $val) {
     $this->_slots[$slot] = $val;
   }
   
-  function rec($slot)
-  {
+  function rec($slot) {
     $this->_slot = $slot;
     ob_start();
   }
   
-  function stop()
-  {
+  function stop() {
     $this->_slots[$this->_slot] = ob_get_clean();
     $this->_slot = null;
   }
   
-  function escaper(\Closure $escaper)
-  {
+  function escaper(\Closure $escaper) {
     $this->_escaper = $escaper;
   }
   
-  function fetch()
-  {
+  function fetch() {
+    $this->_slots = $this->hooks->filter('template.slots', $this, $this->_slots);
     ob_start();
     $this->content();
     return ob_get_clean();
   }
   
-  function content() {}
+  function content() { }
 
-  function escape($val)
-  {
+  function escape($val) {
     return is_string($val) ? call_user_func($this->_escaper, $val) : $val;
   }
 }
+
+class ForwardException extends Exception { }
--- greebo_extra.php
+++ greebo_extra.php
@@ -4,25 +4,10 @@ namespace Greebo;
 
 class BasicContainer extends Container
 {
-  function __construct()
-  {
-    $this->vendor = 'Greebo';
-    $this->app = 'default';
+  function init() {
+    parent::init();
     $this->loader = $this->shared(function($c) { return new ClassLoader; });
-    $this->request = $this->shared(function($c) { return new Request($c); });
     $this->response = $this->shared(function($c) { return new BasicResponse($c); });
-    $this->hooks = $this->shared(function($c) { return new Hooks; });
-    $this->action = $this->request->param('action', 'index');
-    $this->controller = $this->shared(function($c) {
-      $class = sprintf('%s\\Controller\\%s', $c->vendor, $c->app);
-      return new $class($c);
-    });
-    $this->template = $this->shared(function($c) {
-      $class = sprintf('%s\\Template\\%s\\%s', $c->vendor, $c->app, $c->action);
-      $class = new $class($c);
-      $class->escaper(function($val) { return htmlentities($val, ENT_QUOTES, 'utf-8'); });
-      return $class;
-    });
   }
 }
 
@@ -30,8 +15,7 @@ abstract class Bootstrap
 {
   protected $_container;
     
-  function __construct($env)
-  {
+  function __construct($env) {
     $this->_container = $this->container();
     $this->_container->env = $env;
     
@@ -41,27 +25,23 @@ abstract class Bootstrap
   
   abstract function init();
   
-  function container()
-  {
+  function container() {
     return new BasicContainer;
   }
   
-  function run()
-  {
+  function run() {
     try
     {
-      if ($this->_container->loader)
-      {
+      if ($this->_container->loader) {
         $this->_container->loader->registerNamespace($this->_container->vendor, $this->_container->lib_dir);
         $this->_container->loader->register();
       }
     
-      Greebo::create($this->_container)->unleash();
+      $g = new Greebo($this->_container);
+      $g->unleash();
     }
-    catch (Exception $e)
-    {
-      if ($this->_container->debug)
-      {
+    catch (Exception $e) {
+      if ($this->_container->debug) {
         // TODO show backtrace
       }
       else
@@ -74,13 +54,11 @@ abstract class Bootstrap
 
 class Controller2 extends Controller
 {
-  function assign($slot, $val)
-  {
+  function assign($slot, $val) {
     $this->template->$slot = $val;
   }
   
-  function sendback($val)
-  {
+  function sendback($val) {
     $this->response->content($val);
     return false;
   }
@@ -88,13 +66,11 @@ class Controller2 extends Controller
 
 class BasicResponse extends Response
 {
-  function init()
-  {
+  function init() {
     $this->header('Content-type', 'text/html; charset=utf8');
   }
   
-  function redirect($uri)
-  {
+  function redirect($uri) {
     $this->header('Location', $uri);
     $this->content = null;
     $this->send();
@@ -132,10 +108,8 @@ class ClassLoader
    * @param string $namespace   The namespace to use
    * @param string $includePath The path to the namespace
    */
-  public function registerNamespace($namespace, $includePath = null)
-  {
-    if (!isset($this->namespaces[$namespace]))
-    {
+  public function registerNamespace($namespace, $includePath = null) {
+    if (!isset($this->namespaces[$namespace])) {
       $this->namespaces[$namespace] = array();
     }
     $this->namespaces[$namespace][] = $includePath;
@@ -144,16 +118,14 @@ class ClassLoader
   /**
    * Installs this class loader on the SPL autoload stack.
    */
-  public function register()
-  {
+  public function register() {
     spl_autoload_register(array($this, 'loadClass'));
   }
 
   /**
    * Uninstalls this class loader from the SPL autoloader stack.
    */
-  public function unregister()
-  {
+  public function unregister() {
     spl_autoload_unregister(array($this, 'loadClass'));
   }
 
@@ -162,28 +134,24 @@ class ClassLoader
    *
    * @param string $className The name of the class to load
    */
-  public function loadClass($className)
-  {
+  public function loadClass($className) {
     $vendor = substr($className, 0, stripos($className, '\\'));
     if (!$vendor && !isset($this->namespaces[''])) return;
     
     $fileName = '';
     $namespace = '';
-    if (false !== ($lastNsPos = strripos($className, '\\')))
-    {
+    if (false !== ($lastNsPos = strripos($className, '\\'))) {
       $namespace = substr($className, 0, $lastNsPos);
       $className = substr($className, $lastNsPos + 1);
       $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace).DIRECTORY_SEPARATOR;
     }
     $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className).'.php';
 
-    if (!isset($this->namespaces[$vendor]))
-    {
+    if (!isset($this->namespaces[$vendor])) {
       require $fileName;
       return;
     }
-    foreach ($this->namespaces[$vendor] as $dir)
-    {
+    foreach ($this->namespaces[$vendor] as $dir) {
       if (!file_exists($dir.DIRECTORY_SEPARATOR.$fileName)) continue;
       require $dir.DIRECTORY_SEPARATOR.$fileName;
       break;
